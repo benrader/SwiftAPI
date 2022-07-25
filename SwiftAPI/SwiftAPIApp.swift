55de1cfb9af8826dd4fc85065e6b67a34ca0f6da
benrader <benrader@hey.com>
2022-07-25 17:29:09 -0400

    Set up project in Xcode

---
+++ SwiftAPI/SwiftAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAPIApp.swift
+//  SwiftAPI
+//
+//  Created by Ben Rader on 2022-07-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
